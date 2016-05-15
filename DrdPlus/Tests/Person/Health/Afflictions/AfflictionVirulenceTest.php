b2c63daa6b1e41166a02218840282e3f7d01c197
jaroslavtyc <mail@jaroslavtyc.com>
2016-05-15 16:54:30 +0200

    Affliction virulence tested

---
+++ DrdPlus/Tests/Person/Health/Afflictions/AfflictionVirulenceTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace DrdPlus\Tests\Person\Health\Afflictions;
+
+use DrdPlus\Person\Health\Afflictions\AfflictionVirulence;
+use Granam\Tests\Tools\TestWithMockery;
+
+class AfflictionVirulenceTest extends TestWithMockery
+{
+    /**
+     * @test
+     */
+    public function I_can_get_every_virulence()
+    {
+        $roundVirulence = AfflictionVirulence::getRoundVirulence();
+        self::assertInstanceOf(AfflictionVirulence::class, $roundVirulence);
+        self::assertSame('round', $roundVirulence->getValue());
+
+        $minuteVirulence = AfflictionVirulence::getMinuteVirulence();
+        self::assertInstanceOf(AfflictionVirulence::class, $minuteVirulence);
+        self::assertSame('minute', $minuteVirulence->getValue());
+
+        $hourVirulence = AfflictionVirulence::getHourVirulence();
+        self::assertInstanceOf(AfflictionVirulence::class, $hourVirulence);
+        self::assertSame('hour', $hourVirulence->getValue());
+
+        $dayVirulence = AfflictionVirulence::getDayVirulence();
+        self::assertInstanceOf(AfflictionVirulence::class, $dayVirulence);
+        self::assertSame('day', $dayVirulence->getValue());
+    }
+
+    /**
+     * @test
+     * @expectedException \DrdPlus\Person\Health\Afflictions\Exceptions\UnknownVirulencePeriod
+     * @expectedExceptionMessageRegExp ~life~
+     */
+    public function I_can_not_create_custom_virulence()
+    {
+        AfflictionVirulence::getEnum('life');
+    }
+}

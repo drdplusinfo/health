976b7b51a7bc1c3e7019cc46b0f96c62b36dc01e
jaroslavtyc <mail@jaroslavtyc.com>
2016-05-16 11:49:12 +0200

    Affliction by wound test skeleton

---
+++ DrdPlus/Tests/Person/Health/Afflictions/AfflictionByWoundTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace DrdPlus\Tests\Person\Health\Afflictions;
+
+use Granam\Tests\Tools\TestWithMockery;
+
+class AfflictionByWoundTest extends TestWithMockery
+{
+    // TODO
+    protected function setUp()
+    {
+        self::markTestSkipped('Most complex at last');
+    }
+}

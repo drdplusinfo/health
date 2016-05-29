186bf49ce05c89a7c56c30d7575bf09f375713eb
jaroslavtyc <mail@jaroslavtyc.com>
2016-05-29 10:48:50 +0200

    Reason to roll against malus covered by tests

--- DrdPlus/Tests/Person/Health/ReasonToRollAgainstMalusTest.php
+++ DrdPlus/Tests/Person/Health/ReasonToRollAgainstMalusTest.php
@@ -2,10 +2,34 @@
 namespace DrdPlus\Tests\Person\Health;
 
 use DrdPlus\Person\Health\ReasonToRollAgainstMalus;
-use Granam\Tests\Tools\TestWithMockery;
 
-class ReasonToRollAgainstMalusTest extends TestWithMockery
+class ReasonToRollAgainstMalusTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @test
+     */
+    public function I_can_use_wound_reason()
+    {
+        $woundReason = ReasonToRollAgainstMalus::getWoundReason();
+        self::assertInstanceOf(ReasonToRollAgainstMalus::class, $woundReason);
+        self::assertTrue($woundReason->becauseOfWound());
+        self::assertFalse($woundReason->becauseOfHeal());
+        self::assertSame('wound', $woundReason->getValue());
+        self::assertSame('wound', ReasonToRollAgainstMalus::WOUND);
+        self::assertSame(ReasonToRollAgainstMalus::getIt('wound'), $woundReason);
+    }
+
+    public function I_can_use_heal_reason()
+    {
+        $healReason = ReasonToRollAgainstMalus::getHealReason();
+        self::assertInstanceOf(ReasonToRollAgainstMalus::class, $healReason);
+        self::assertTrue($healReason->becauseOfHeal());
+        self::assertFalse($healReason->becauseOfWound());
+        self::assertSame('heal', $healReason->getValue());
+        self::assertSame('heal', ReasonToRollAgainstMalus::HEAL);
+        self::assertSame(ReasonToRollAgainstMalus::getIt('heal'), $healReason);
+    }
+
     /**
      * @test
      * @expectedException \DrdPlus\Person\Health\Exceptions\UnknownReasonToRollAgainstMalus

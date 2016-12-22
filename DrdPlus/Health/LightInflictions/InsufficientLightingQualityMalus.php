cd4632b52df975366391f26678a5501d090beec7
Jaroslav Týc <mail@jaroslavtyc.com>
2016-12-22 10:20:57 +0100

    Race light sensitivity si taken into account for insufficient lighting quality malus

--- DrdPlus/Health/LightInflictions/InsufficientLightingQualityMalus.php
+++ DrdPlus/Health/LightInflictions/InsufficientLightingQualityMalus.php
@@ -1,6 +1,7 @@
 <?php
 namespace DrdPlus\Health\LightInflictions;
 
+use DrdPlus\Codes\RaceCode;
 use Granam\Integer\NegativeInteger;
 use Granam\Strict\Object\StrictObject;
 
@@ -13,14 +14,20 @@ class InsufficientLightingQualityMalus extends StrictObject implements NegativeI
 
     /**
      * @param LightingQuality $currentLightingQuality
+     * @param RaceCode $raceCode
      */
-    public function __construct(LightingQuality $currentLightingQuality)
+    public function __construct(LightingQuality $currentLightingQuality, RaceCode $raceCode)
     {
         $this->malus = 0;
         if ($currentLightingQuality->getValue() < -10) {
             /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
             $contrast = new Contrast(new LightingQuality(0), $currentLightingQuality);
             $possibleMalus = -$contrast->getValue();
+            if (in_array($raceCode->getValue(), [RaceCode::DWARF, RaceCode::ORC], true)) {
+                $possibleMalus += 4;
+            } else if ($raceCode->getValue() === RaceCode::KROLL) {
+                $possibleMalus += 2;
+            }
             if ($possibleMalus >= -20) {
                 $this->malus = $possibleMalus;
             } else {
--- DrdPlus/Tests/Health/LightInflictions/InsufficientLightingQualityMalusTest.php
+++ DrdPlus/Tests/Health/LightInflictions/InsufficientLightingQualityMalusTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace DrdPlus\Health\LightInflictions;
 
+use DrdPlus\Codes\RaceCode;
 use Granam\Tests\Tools\TestWithMockery;
 
 class InsufficientLightingQualityMalusTest extends TestWithMockery
@@ -9,12 +10,18 @@ class InsufficientLightingQualityMalusTest extends TestWithMockery
      * @test
      * @dataProvider provideLightingQualityAndExpectedMalus
      * @param int $lightingQualityValue
+     * @param string $raceValue
      * @param int $expectedMalus
      */
-    public function I_get_malus_from_insufficient_light($lightingQualityValue, $expectedMalus)
+    public function I_get_malus_from_insufficient_light(
+        $lightingQualityValue,
+        $raceValue,
+        $expectedMalus
+    )
     {
         $insufficientLightingQualityMalus = new InsufficientLightingQualityMalus(
-            new LightingQuality($lightingQualityValue)
+            new LightingQuality($lightingQualityValue),
+            RaceCode::getIt($raceValue)
         );
         self::assertSame($expectedMalus, $insufficientLightingQualityMalus->getValue());
         self::assertSame((string)$expectedMalus, (string)$insufficientLightingQualityMalus);
@@ -23,14 +30,17 @@ class InsufficientLightingQualityMalusTest extends TestWithMockery
     public function provideLightingQualityAndExpectedMalus()
     {
         return [
-            [0, 0],
-            [-10, 0],
-            [-11, -1],
-            [-19, -1],
-            [-20, -2],
-            [-100, -10],
-            [-200, -20],
-            [-999, -20], // maximum is -20
+            [0, RaceCode::HUMAN, 0],
+            [-10, RaceCode::ELF, 0],
+            [-11, RaceCode::HOBBIT, -1],
+            [-19, RaceCode::HUMAN, -1],
+            [-20, RaceCode::HUMAN, -2],
+            [-59, RaceCode::ELF, -5],
+            [-59, RaceCode::ORC, -1],
+            [-59, RaceCode::DWARF, -1],
+            [-100, RaceCode::HOBBIT, -10],
+            [-200, RaceCode::HOBBIT, -20],
+            [-999, RaceCode::DWARF, -20], // maximum is -20
         ];
     }
 }

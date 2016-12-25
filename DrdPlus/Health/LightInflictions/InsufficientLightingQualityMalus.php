ba40630a68788877dd27ca474383ccbe7761ba74
Jaroslav Týc <mail@jaroslavtyc.com>
2016-12-25 09:57:45 +0100

    Dusk sight skill is taken into account for malus from insufficient lighting

--- DrdPlus/Health/LightInflictions/InsufficientLightingQualityMalus.php
+++ DrdPlus/Health/LightInflictions/InsufficientLightingQualityMalus.php
@@ -2,11 +2,13 @@
 namespace DrdPlus\Health\LightInflictions;
 
 use DrdPlus\Codes\RaceCode;
+use DrdPlus\Skills\Combined\DuskSight;
 use Granam\Integer\NegativeInteger;
 use Granam\Strict\Object\StrictObject;
 
 /**
  * See PPH page 128
+ * Note about Orcs: they can have malus even from too strong lighting, despite the class name "insufficient".
  */
 class InsufficientLightingQualityMalus extends StrictObject implements NegativeInteger
 {
@@ -18,8 +20,13 @@ class InsufficientLightingQualityMalus extends StrictObject implements NegativeI
     /**
      * @param LightingQuality $currentLightingQuality
      * @param RaceCode $raceCode
+     * @param DuskSight $duskSight
      */
-    public function __construct(LightingQuality $currentLightingQuality, RaceCode $raceCode)
+    public function __construct(
+        LightingQuality $currentLightingQuality,
+        RaceCode $raceCode,
+        DuskSight $duskSight
+    )
     {
         $this->malus = 0;
         if ($currentLightingQuality->getValue() < -10) {
@@ -27,14 +34,15 @@ class InsufficientLightingQualityMalus extends StrictObject implements NegativeI
             $contrast = new Contrast(new LightingQuality(0), $currentLightingQuality);
             $possibleMalus = -$contrast->getValue();
             if (in_array($raceCode->getValue(), [RaceCode::DWARF, RaceCode::ORC], true)) {
-                $possibleMalus += 4;
+                $possibleMalus += 4; // lowering malus
             } else if ($raceCode->getValue() === RaceCode::KROLL) {
-                $possibleMalus += 2;
+                $possibleMalus += 2; // lowering malus
             }
+            $possibleMalus += $duskSight->getInsufficientLightingBonus(); // lowering malus
             if ($possibleMalus >= -20) {
                 $this->malus = $possibleMalus;
             } else {
-                $this->malus = -20;
+                $this->malus = -20; // maximal possible malus on absolute dark, see PPH page 128 right column bottom
             }
         } else if ($currentLightingQuality->getValue() >= 60 /* strong daylight */ && $raceCode->getValue() === RaceCode::ORC) {
             // see PPH page 128 right column bottom
--- DrdPlus/Tests/Health/LightInflictions/InsufficientLightingQualityMalusTest.php
+++ DrdPlus/Tests/Health/LightInflictions/InsufficientLightingQualityMalusTest.php
@@ -2,6 +2,7 @@
 namespace DrdPlus\Health\LightInflictions;
 
 use DrdPlus\Codes\RaceCode;
+use DrdPlus\Skills\Combined\DuskSight;
 use Granam\Tests\Tools\TestWithMockery;
 
 class InsufficientLightingQualityMalusTest extends TestWithMockery
@@ -11,17 +12,20 @@ class InsufficientLightingQualityMalusTest extends TestWithMockery
      * @dataProvider provideLightingQualityAndExpectedMalus
      * @param int $lightingQualityValue
      * @param string $raceValue
+     * @param int $fromDuskSightBonus
      * @param int $expectedMalus
      */
     public function I_get_malus_from_insufficient_light(
         $lightingQualityValue,
         $raceValue,
+        $fromDuskSightBonus,
         $expectedMalus
     )
     {
         $insufficientLightingQualityMalus = new InsufficientLightingQualityMalus(
             new LightingQuality($lightingQualityValue),
-            RaceCode::getIt($raceValue)
+            RaceCode::getIt($raceValue),
+            $this->createDuskSight($fromDuskSightBonus)
         );
         self::assertSame($expectedMalus, $insufficientLightingQualityMalus->getValue());
         self::assertSame((string)$expectedMalus, (string)$insufficientLightingQualityMalus);
@@ -29,22 +33,38 @@ class InsufficientLightingQualityMalusTest extends TestWithMockery
 
     public function provideLightingQualityAndExpectedMalus()
     {
+        // note: orcs and dwarfs have +4 bonus in darkness, krolls +2 but orcs have -2 malus on bright light
         return [
-            [0, RaceCode::HUMAN, 0],
-            [-10, RaceCode::ELF, 0],
-            [-11, RaceCode::HOBBIT, -1],
-            [-19, RaceCode::HUMAN, -1],
-            [-20, RaceCode::HUMAN, -2],
-            [-59, RaceCode::ELF, -5],
-            [-59, RaceCode::KROLL, -3],
-            [-59, RaceCode::ORC, -1],
-            [-59, RaceCode::DWARF, -1],
-            [-100, RaceCode::HOBBIT, -10],
-            [-200, RaceCode::HOBBIT, -20],
-            [-999, RaceCode::DWARF, -20], // maximum is -20
-            [60, RaceCode::KROLL, 0],
-            [59, RaceCode::ORC, 0],
-            [60, RaceCode::ORC, -2],
+            [0, RaceCode::HUMAN, 0, 0],
+            [-10, RaceCode::ELF, 0, 0],
+            [-11, RaceCode::HOBBIT, 0, -1],
+            [-11, RaceCode::HOBBIT, 1, 0],
+            [-19, RaceCode::HUMAN, 0, -1],
+            [-20, RaceCode::HUMAN, 0, -2],
+            [-59, RaceCode::ELF, 0, -5],
+            [-59, RaceCode::KROLL, 0, -3],
+            [-59, RaceCode::ORC, 0, -1],
+            [-59, RaceCode::DWARF, 0, -1],
+            [-100, RaceCode::HOBBIT, 0, -10],
+            [-200, RaceCode::HOBBIT, 0, -20],
+            [-200, RaceCode::ORC, 3, -13],
+            [-999, RaceCode::DWARF, 90, -5],
+            [-999, RaceCode::DWARF, 0, -20], // maximum is -20
+            [60, RaceCode::KROLL, 0, 0],
+            [59, RaceCode::ORC, 0, 0],
+            [60, RaceCode::ORC, 0,-2],
         ];
+    }
+
+    /**
+     * @param int $bonus
+     * @return \Mockery\MockInterface|DuskSight
+     */
+    private function createDuskSight($bonus)
+    {
+        $duskSight = $this->mockery(DuskSight::class);
+        $duskSight->shouldReceive('getInsufficientLightingBonus')
+            ->andReturn($bonus);
+        return $duskSight;
     }
 }
